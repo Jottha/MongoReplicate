8318b806c34b27697fc58339a8d3b5ebe28eb93b
Jailton Ventura <jottha_2006@hotmail.com>
2019-05-05 16:00:02 -0300

    refatorando estrutura

--- setup/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo ************************************************
-echo Starting the replica set
-echo ************************************************
-
-sleep 10 | echo Sleeping
-mongo mongodb://mongo:27017 replicaSet.js
-mongo mongodb://arbiter:30000 arbiterSet.js
