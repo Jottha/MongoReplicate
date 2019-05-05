051f2709684de851dff5b1328bc17014dd3ecc14
Jailton Ventura <jottha_2006@hotmail.com>
2019-05-05 15:41:12 -0300

    Modificação na estrutura

--- setup/setup.sh
+++ setup/setup.sh
@@ -4,5 +4,5 @@ echo Starting the replica set
 echo ************************************************
 
 sleep 10 | echo Sleeping
-mongo mongodb://mongo-rs0-1:27017 replicaSet.js
-mongo mongodb://arbiter-ab0-1:30000 arbiterSet.js
+mongo mongodb://mongo:27017 replicaSet.js
+mongo mongodb://arbiter:30000 arbiterSet.js
